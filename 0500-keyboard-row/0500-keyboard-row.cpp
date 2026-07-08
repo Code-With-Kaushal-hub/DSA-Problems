ac6e90d406169894f424d170a629b13f54511e5f
Kaushal Maurya <kaushalmaurya055@gmail.com>
2026-07-08 19:34:20 +0530

    Time: 0 ms (100%), Space: 9.2 MB (49.57%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    vector<string> findWords(vector<string>& words) {
+        string a = "qwertyuiop";
+        string b = "asdfghjkl";
+        string c = "zxcvbnm";
+
+        vector<string> ans;
+
+        for (string i : words) {
+            int t = 0;
+            int x = 0;
+
+            for (char j : i) {
+                if (a.find(tolower(j)) == string::npos) {
+                    t = 1;
+                    break;
+                } else {
+                    x = 1;
+                }
+            }
+
+            if (x == 1 && t == 0) {
+                ans.push_back(i);
+                continue;
+            }
+
+            t = 0;
+            x = 0;
+
+            for (char j : i) {
+                if (b.find(tolower(j)) == string::npos) {
+                    t = 1;
+                    break;
+                } else {
+                    x = 1;
+                }
+            }
+
+            if (x == 1 && t == 0) {
+                ans.push_back(i);
+                continue;
+            }
+
+            t = 0;
+            x = 0;
+
+            for (char j : i) {
+                if (c.find(tolower(j)) == string::npos) {
+                    t = 1;
+                    break;
+                } else {
+                    x = 1;
+                }
+            }
+
+            if (x == 1 && t == 0) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
