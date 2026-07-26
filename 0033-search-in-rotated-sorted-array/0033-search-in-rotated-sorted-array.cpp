89de601d98c8adf2f9e815b9a699a98909c3c5f7
Kaushal Maurya <kaushalmaurya055@gmail.com>
2026-07-26 11:57:12 +0530

    Time: 0 ms (100%), Space: 15.2 MB (36.47%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,11 +1,66 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                return i;
-            }
+
+    int binarySearch(vector<int>& nums, int low, int high, int target) {
+
+        while(low <= high) {
+
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] == target)
+                return mid;
+
+            if(nums[mid] > target)
+                high = mid-1;
+            else
+                low = mid+1;
         }
+
         return -1;
+    }
+
+
+    int search(vector<int>& nums, int target) {
+
+        int n = nums.size();
+
+int low = 0;
+int high = n - 1;
+
+// find minimum element index
+while(low < high) {
+    int mid = low + (high - low) / 2;
+
+    if(nums[mid] > nums[high]) {
+        low = mid + 1;
+    }
+    else {
+        high = mid;
+    }
+}
+
+int pivot = low;
+
+// largest element index
+int peak;
+
+if(pivot == 0)
+    peak = n - 1;
+else
+    peak = pivot - 1;
+
+        // Search left side
+        if(target >= nums[0] && target <= nums[peak]) {
+
+            return binarySearch(nums,0,peak,target);
+
+        }
+
+        // Search right side
+        else {
+
+            return binarySearch(nums,peak+1,nums.size()-1,target);
+
+        }
     }
 };
